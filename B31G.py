1cfefe9e912250b8275ac81e8a444af8c99220a1
Ryan Rogers <rtrogers89@gmail.com>
2022-08-02 07:34:11 -0600

    Aug 1, 2022 - ComboBox and working on combovariable to button

--- B31G.py
+++ B31G.py
@@ -1,8 +1,10 @@
 from tkinter import *
 import getExcel
 from functools import partial
+from tkinter import ttk
 
-global vendorDropDrown
+global vendorSelect2
+
 
 
 root=Tk()
@@ -12,26 +14,35 @@ width=root.winfo_screenwidth()
 height=root.winfo_screenheight()
 #root.geometry("%dx%d" % (width, height))
 
-
+#callback for vendor
+def callbackFunc(event):
+    vendorDropDown=event.get()
+    print(vendorDropDown)
 
 # Create a dropdown list of vendors for formatting purposes.
 options=[
+    "Test",
     "OnStream",
     "Encompass",
     "Rosen"]
-vendorSelect=StringVar(root)
-vendorSelect.set(options[0])    #default vendor
-vendorDropDown=OptionMenu(root, vendorSelect, *options)
+
+#
+vendorDropDown=ttk.Combobox(root, value=options)
+vendorDropDown.current(0)
+vendorDropDown.bind("<<ComboboxSelected>>", command=callbackFunc)
 vendorDropDown.grid(row=0, column=0)
 
 # Use this to pass function through button
-action_with_arg = partial(getExcel.importILIData,vendorSelect.get())
-
-                                         
-
+action_with_arg = partial(getExcel.vendorCheck,vendorDropDown.get())
+                                             
 # Create a button for uploading excel data
 upload=Button(root, text = "Upload File", command = action_with_arg)
 upload.grid(row=1,column=1)
+
+#
+review=Button(root,text="Submit", command=action_with_arg)
+review.grid(row=2,column=2)
+
 
 
 
--- getExcel.py
+++ getExcel.py
@@ -5,10 +5,15 @@ from tkinter import filedialog as fd
 #Need to install 'pandas', 'openpyxl'
 
 
-def importILIData(vendor):
+def importILIData():
     #Allows the user to select file to open.
-    file = pd.read_excel(fd.askopenfilename())
-    if vendor == "OnStream":
+    file = fd.askopenfilename()
+    
+
+def vendorCheck(vendor,file):
+    if vendor == "Test":
+        Test(file)
+    elif vendor == "OnStream":
         OnStream(file)
     elif vendor == "Encompass":
         Encompass(file)
@@ -16,21 +21,26 @@ def importILIData(vendor):
         Rosen(file)
 
 
+def Test(file):
+    print(pd.read_excel(file))
+    print('------------------------------')
+    Testfile = pd.read_excel(file,usecols=[0,1,2,3]).to_dict(orient='dict') #you can use column names as well, or index location like this.
+    print(Testfile.keys())
+    print('------------------------------')
+    print(Testfile.values())
+    print('------------------------------')
+    column1=Testfile['Unnamed: 0']
+    print(column1[0])
+
+
+
 def OnStream(file):
-    #print("OnStream Printed")
+    print("OnStream Printed")
     
 def Rosen(file):
-    #print("Rosen Printed")
+    print("Rosen Printed")
 
 def Encompass(file):
-    #print("Encompass Printed")
-
-
-
-  
-    #sortFile(file)
-
-
-
-
-
+    print('------------------------------')
+    Testfile = pd.read_excel(file,sheet_name="Anomalies & Features Listing",usecols=[1,2,3,4,5,6,7,8,10,11,12,13,16,19,20,21,24]).to_dict(orient='dict') #you can use column names as well, or index location like this.
+    #print(Testfile.keys())
